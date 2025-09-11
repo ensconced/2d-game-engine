f5ed886ad60df5f62b21dcdab42cf04b8c63a8ed
Joe Barnett <joe@joebarnett.xyz>
2025-09-11 16:06:13 +0100

    more flake setup

--- flake.nix
+++ flake.nix
@@ -10,12 +10,20 @@
     in {
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
-	  gcc
-	  SDL2
-	  SDL2_image
-	  SDL2_ttf
-	  SDL2_mixer
-	  lua5_3
+	  # The buildEnv trick here is a way of ensuring that
+	  # all the man outputs etc are included in the devshell
+	  (buildEnv {
+	    name = "devShell";
+	    paths = [
+              pkg-config
+              gcc
+              SDL2
+              SDL2_image
+              SDL2_ttf
+              SDL2_mixer
+              lua5_3
+            ];
+	  })
         ];
       };
     };
